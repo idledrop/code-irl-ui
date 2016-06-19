4c70ac81b7eba3ce85b4094857a68c3c02df2a0b
Jonathan Craig <Jonathan Craig>
2016-06-18 22:11:44 -0400

    Added Tag List 2

--- src/app/shared/tags/tag-list.model.ts
+++ src/app/shared/tags/tag-list.model.ts
@@ -2,10 +2,25 @@ import { BehaviorSubject } from 'rxjs/Rx';
 
 export class TagList{
   private tags:any[];
-  tags$:Observable<any[]>;
+  tags$:BehaviorSubject<any[]>;
 
   constructor(){
     this.tags = [];
     this.tags$ = new BehaviorSubject([]);
+  }
+
+  removeTag(id:number){
+    this.tags = this.tags.filter(tag => tag.id !== id);
+    this.tags$.next(this.tags);
+  }
+
+  addTag(tag:any){
+    this.tags.push(tag);
+    this.tags$.next(this.tags);
+  }
+
+  resetTags(){
+    this.tags = [];
+    this.tags$.next(this.tags);
   }
 }
